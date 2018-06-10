355d5e4f70866775381c213c39e96a43ca1b1600
Aldo Borrero <aldo@aldoborrero.com>
2018-06-10 19:18:30 +0200

    Fixed formatting in cacheDB file

--- src/vm/cacheDB.ts
+++ src/vm/cacheDB.ts
@@ -1,32 +1,39 @@
 import * as Redis from 'ioredis'
 import * as rpc from 'json-rpc2'
+
 interface ImyCallbackType { (err: Error, result: any): any }
+
 interface IencOptions {
 	keyEncoding: string;
 	valueEncoding: string;
 }
+
 interface IrpcOptions {
 	port: number;
 	host: string;
 }
+
 interface IputValues {
 	type: string,
 	key: Buffer,
 	value: Buffer
 }
+
 class CacheDB {
 	redisConn: any;
 	rpcConn: any;
-	curState: string;
+  curState: string;
+
 	constructor (_redis: string, _rpc: IrpcOptions) {
 		this.redisConn = new Redis(_redis)
 		this.rpcConn = rpc.Client.$create(_rpc.port, _rpc.host)
-	}
+  }
+
 	get(key: Buffer, options: IencOptions, cb: ImyCallbackType) {
 		let _this = this
 		this.redisConn.get(key, (err: Error, result: string) => {
 			if (!err && result) {
- 				cb(null, new Buffer(result, 'hex'))
+        cb(null, new Buffer(result, 'hex'))
 			}
 			else {
 				this.rpcConn.call('eth_getKeyValue', ['0x' + key.toString('hex')], function(err: Error, result: string) {
@@ -41,7 +48,8 @@ class CacheDB {
 				})
 			}
 		})
-	}
+  }
+
 	put(key: Buffer, val: Buffer, options: IencOptions, cb: ImyCallbackType){
 		let _this = this
 		this.redisConn.set(key,val, (err: Error, result: string) => {
@@ -64,10 +72,17 @@ class CacheDB {
 				}
 			})}
 
-	batch(ops: Array<IputValues>, options: IencOptions, cb: ImyCallbackType){ 
+	batch(ops: Array<IputValues>, options: IencOptions, cb: ImyCallbackType){
 		cb(null, true)
-	}
-	del(key: Buffer, cb: ImyCallbackType) { cb(null, true) }
-	isOpen():boolean { return true}
+  }
+
+	del(key: Buffer, cb: ImyCallbackType) {
+    cb(null, true)
+  }
+
+  isOpen():boolean {
+    return true
+  }
 }
+
 export default CacheDB
