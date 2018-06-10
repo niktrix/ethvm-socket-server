b29cfd3dbc9287890db5b93a2af4dbb46f27bcbf
Aldo Borrero <aldo@aldoborrero.com>
2018-06-10 19:17:55 +0200

    Added forgotten chartlayout to charts

--- src/typeLayouts/index.ts
+++ src/typeLayouts/index.ts
@@ -2,9 +2,12 @@ import blockLayout from '@/typeLayouts/blockLayout'
 import txLayout from '@/typeLayouts/txLayout'
 import txLogLayout from '@/typeLayouts/txLogLayout'
 import traceLayout from '@/typeLayouts/traceLayout'
+import ChartLayout from '@/typeLayouts/chart'
+
 export {
 	blockLayout,
 	txLayout,
 	txLogLayout,
-	traceLayout
+  traceLayout,
+  ChartLayout
 }
