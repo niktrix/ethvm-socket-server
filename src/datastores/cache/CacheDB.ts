5b4fe2431f6dd61656283a57f5e44d27423b3a1c
Aldo Borrero González <aldo@aldoborrero.com>
2018-06-15 17:41:39 +0200

    Moved cacheDB from vm to datastore

---
+++ src/datastores/cache/CacheDB.ts
@@ -0,0 +1,66 @@
+import * as Redis from 'ioredis'
+import * as rpc from 'json-rpc2'
+import { Callback } from "@/interfaces"
+
+interface IencOptions {
+  keyEncoding: string
+  valueEncoding: string
+}
+
+export interface CacheDBOptions {
+  redisUrl: string,
+  rpcHost: string,
+  rpcPort: number,
+}
+
+export class CacheDB {
+  private readonly r: any
+  private readonly rpc: any
+
+  constructor(opts : CacheDBOptions) {
+    this.r = new Redis(opts.redisUrl)
+    this.rpc = rpc.Client.$create(opts.rpcPort, opts.rpcHost)
+  }
+
+  get(key: Buffer, options: IencOptions, cb: Callback) {
+    this.r.get(key, (err: Error, result: string) => {
+      if (!err && result) {
+        cb(null, new Buffer(result, 'hex'))
+      } else {
+        this.rpc.call(
+          'eth_getKeyValue',
+          ['0x' + key.toString('hex')],
+          function(err: Error, result: string) {
+            if (err) {
+              cb(err, null)
+            } else {
+              const resBuf: Buffer = new Buffer(result.substring(2), 'hex')
+              this.r.set(key, resBuf.toString('hex'))
+              cb(null, resBuf)
+            }
+          }
+        )
+      }
+    })
+  }
+
+  put(key: Buffer, val: Buffer, options: IencOptions, cb: Callback) {
+    this.r.set(key, val, (err: Error, result: string) => {
+      if (!err && result) {
+        cb(null, new Buffer(result, 'hex'))
+      } else {
+        cb(err, null)
+      }
+    })
+  }
+
+  getString(key: Buffer, options: IencOptions, cb: Callback) {
+    this.r.get(key, (err: Error, result: string) => {
+      if (!err && result) {
+        cb(null, result)
+      } else {
+        cb(err, null)
+      }
+    })
+  }
+}
