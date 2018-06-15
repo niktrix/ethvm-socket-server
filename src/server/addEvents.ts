ba8a0b8be2dbf2f329c89244996ef7bcb66398f8
Aldo Borrero González <aldo@aldoborrero.com>
2018-06-15 17:45:51 +0200

    Formatted and simplified logic on addEvents

--- src/server/addEvents.ts
+++ src/server/addEvents.ts
@@ -1,201 +1,260 @@
-import config from '@/config'
 import ds from '@/datastores'
 import RethinkDBDataStore from '@/datastores/providers/RethinkDBDataStore'
+import { CacheDB } from '@/datastores/cache'
 import { l } from '@/helpers'
 import { common } from '@/libs'
 import { BlockModel, TxModel } from '@/models'
-import CacheDb from '@/vm/cacheDB'
-import VmRunner from '@/vm/vmRunner'
+import { VmRunner } from '@/vm/'
 import fetch from 'node-fetch'
 import * as SocketIO from 'socket.io'
+import { Callback } from '@/interfaces'
 
-const redisUrl = config.get('eth_vm_server.data_stores.redis.url')
-const host = config.get('eth_vm_server.geth.host')
-const port = config.get('eth_vm_server.geth.port')
-
-type CallbackFunction = (err: Error, result: any) => any
-
-interface Iinstances {
-  rdb: RethinkDBDataStore;
-  vmR: VmRunner;
-  vmE: any;
+interface Instances {
+  rdb: RethinkDBDataStore
+  vmR: VmRunner
+  vmE: any
+  cacheDB: CacheDB
 }
 
-interface _event {
-  name: string,
-  onEvent: (_socket: SocketIO.Socket, _msg: string, _glob?: Iinstances, _cb?: CallbackFunction) => void
+interface SocketIOEvent {
+  name: string
+  onEvent: (
+    socket: SocketIO.Socket,
+    msg: string,
+    glob?: Instances,
+    cb?: Callback
+  ) => void
 }
 
-let cacheDB = new CacheDb(redisUrl, {
-  port,
-  host
-})
-
-const events: Array<_event> = [{
-  name: "join",
-  onEvent: (_socket, _msg): void => {
-    if (_msg) {
-      _socket.join(_msg)
-      l.info(_socket.id, "joined", _msg)
-    } else {
-      l.error(_socket.id, 'tried to join invalid room', _msg)
-    }
-  }
-}, {
-  name: "leave",
-  onEvent: (_socket, _msg): void => {
-    if (_msg) {
-      _socket.leave(_msg)
-      l.info(_socket.id, "Left", _msg)
-    } else {
-      l.error(_socket.id, 'tried to leave invalid room', _msg)
-    }
-  }
-}, {
-  name: "pastBlocks",
-  onEvent: (_socket, _msg, _glob, _cb): void => {
-    ds.getBlocks((_blocks: Array<BlockModel>) => {
-      let blocks: Array<BlockModel> = []
-      _blocks.forEach((_block: BlockModel, idx: number): void => {
-        blocks.unshift(_block)
-      })
-      _cb(null, blocks)
-    })
-  }
-}, {
-  name: "pastTxs",
-  onEvent: (_socket, _msg, _glob, _cb) => {
-    ds.getTransactions((_txs: Array<TxModel>) => {
-      let txs: Array<TxModel> = []
-      _txs.forEach((_tx) => {
-        txs.unshift(_tx)
-      })
-      _cb(null, txs)
-    })
-  }
-}, {
-  name: "getBlock",
-  onEvent: (_socket, _msg, _glob, _cb): void => {
-    _glob.rdb.getBlock(_msg, _cb)
-  }
-}, {
-  name: "getTx",
-  onEvent: (_socket, _msg, _glob, _cb): void => {
-    _glob.rdb.getTx(_msg, _cb)
-  }
-}, {
-  name: "getBlockTransactions",
-  onEvent: (_socket, _msg, _glob, _cb): void => {
-    _glob.rdb.getBlockTransactions(_msg, _cb)
-  }
-}, {
-  name: "getBalance",
-  onEvent: (_socket, _msg, _glob, _cb): void => {
-    //_glob.vmR.getAccount(_msg, _cb)
-    _glob.vmE.getBalance(_msg, _cb)
-  }
-}, {
-  name: "getTokenBalance",
-  onEvent: (_socket, _msg, _glob, _cb): void => {
-    _glob.vmE.getAllTokens(_msg, _cb)
-  }
-},
-{
-  name: "getTotalTxs",
-  onEvent: (_socket, _msg, _glob, _cb): void => {
-    _glob.rdb.getTotalTxs(_msg, _cb)
-  }
-},
-{
-  name: "getTxs",
-  onEvent: (_socket, _msg, _glob, _cb): void => {
-    _glob.rdb.getTxsOfAddress(_msg, _cb)
-  }
-},
-{
-  name: "getChartsData",
-  onEvent: (_socket, _msg, _glob, _cb): void => {
-    _glob.rdb.getChartsData(_cb)
-  }
-},
-{
-  name: "ethCall",
-  onEvent: (_socket, _msg: any, _glob, _cb): void => {
-
-    _glob.vmR.call(_msg, _cb)
-  }
-}, {
-  name: "getKeyValue",
-  onEvent: (_socket, _msg: any, _glob, _cb): void => {
-    if (!common.check.isBufferObject(_msg, 32)) _cb(common.newError(common.errors.notBuffer), null)
-    else _glob.vmR.getKeyValue(_msg, _cb)
-  }
-}, {
-  name: "getCurrentStateRoot",
-  onEvent: (_socket, _msg: any, _glob, _cb): void => {
-    if (_msg != "") _cb(common.newError(common.errors.invalidInput), null)
-    else _glob.vmR.getCurrentStateRoot(_cb)
-  }
-},
-{
-  name: "getEthToUSD",
-  onEvent: (_socket, _msg: any, _glob, _cb): void => {
-    let _this = this
-
-    cacheDB.getString(new Buffer("Iethtousd"), {
-      keyEncoding: 'binary',
-      valueEncoding: 'binary'
-    }, function (err: Error, result: any) {
-      if (err == null) {
-        console.log("EthtoUSD is in cache get ")
-        _cb(err, result)
-      } else {
-        console.log("EthtoUSD getting from api")
-        getEthToUSD(function (err, data) {
-          console.log("data", data[0].price_usd)
-          cacheDB.put(new Buffer("Iethtousd"), new Buffer(data[0].price_usd), {
-            keyEncoding: 'binary',
-            valueEncoding: 'binary'
-          }, function (err: Error, result: any) {
-            _cb(err, result)
-          })
-          _cb(err, result)
-        });
+const events: Array<SocketIOEvent> = [
+  {
+    name: 'join',
+    onEvent: (socket, msg): void => {
+      if (!msg) {
+        l.error(socket.id, 'tried to join invalid room', msg)
+        return
       }
-    });
-  }
-},
-{
-  name: "getTransactionPages",
-  onEvent: (_socket, reqObj: any, _glob, _cb): void => {
-    if (reqObj.hash && (!common.check.isBufferObject(reqObj.hash, 32) || !common.check.isNumber(reqObj.number))) _cb(common.newError(common.errors.notBuffer), null)
-    else _glob.rdb.getTransactionPages(reqObj.hash, reqObj.number, _cb)
-  }
-}, {
-  name: "getAddressTransactionPages",
-  onEvent: (_socket, reqObj: any, _glob, _cb): void => {
-    if (reqObj.hash && (!common.check.isBufferObject(reqObj.hash, 32) || !common.check.isNumber(reqObj.number))) _cb(common.newError(common.errors.notBuffer), null)
-    else if (!common.check.isBufferObject(reqObj.address, 20)) _cb(common.newError(common.errors.notBuffer), null)
-    else _glob.rdb.getAddressTransactionPages(reqObj.address, reqObj.hash, reqObj.number, _cb)
-  }
-}]
 
-let onConnection = (_socket: SocketIO.Socket, _rdb: RethinkDBDataStore, _vmR: VmRunner, _vmE: any) => {
-  events.forEach((event: _event, idx: number) => {
-    _socket.on(event.name, (msg: any, cb: CallbackFunction) => {
-      event.onEvent(_socket, msg, {
-        rdb: _rdb,
-        vmR: _vmR,
-        vmE: _vmE
-      }, cb)
+      socket.join(msg)
+      l.info(socket.id, 'joined', msg)
+    }
+  },
+  {
+    name: 'leave',
+    onEvent: (socket, msg): void => {
+      if (!msg) {
+        l.error(socket.id, 'tried to leave invalid room', msg)
+        return
+      }
+
+      socket.leave(msg)
+      l.info(socket.id, 'Left', msg)
+    }
+  },
+  {
+    name: 'pastBlocks',
+    onEvent: (socket, msg, glob, cb): void => {
+      ds.getBlocks((_blocks: Array<BlockModel>) => {
+        let blocks: Array<BlockModel> = []
+        blocks.forEach(
+          (block: BlockModel, idx: number): void => {
+            blocks.unshift(block)
+          }
+        )
+        cb(null, blocks)
+      })
+    }
+  },
+  {
+    name: 'pastTxs',
+    onEvent: (socket, msg, glob, cb): void => {
+      ds.getTransactions((_txs: Array<TxModel>) => {
+        let txs: Array<TxModel> = []
+        _txs.forEach(_tx => {
+          txs.unshift(_tx)
+        })
+        cb(null, txs)
+      })
+    }
+  },
+  {
+    name: 'getBlock',
+    onEvent: (socket, msg, glob, cb): void => {
+      glob.rdb.getBlock(msg, cb)
+    }
+  },
+  {
+    name: 'getTx',
+    onEvent: (socket, msg, glob, cb): void => {
+      glob.rdb.getTx(msg, cb)
+    }
+  },
+  {
+    name: 'getBlockTransactions',
+    onEvent: (socket, msg, glob, cb): void => {
+      glob.rdb.getBlockTransactions(msg, cb)
+    }
+  },
+  {
+    name: 'getBalance',
+    onEvent: (socket, msg, glob, cb): void => {
+      //_glob.vmR.getAccount(_msg, _cb)
+      glob.vmE.getBalance(msg, cb)
+    }
+  },
+  {
+    name: 'getTokenBalance',
+    onEvent: (socket, msg, glob, cb): void => {
+      glob.vmE.getAllTokens(msg, cb)
+    }
+  },
+  {
+    name: 'getTotalTxs',
+    onEvent: (socket, msg, glob, cb): void => {
+      glob.rdb.getTotalTxs(msg, cb)
+    }
+  },
+  {
+    name: 'getTxs',
+    onEvent: (socket, msg, glob, cb): void => {
+      glob.rdb.getTxsOfAddress(msg, cb)
+    }
+  },
+  {
+    name: 'getChartsData',
+    onEvent: (socket, msg, glob, cb): void => {
+      glob.rdb.getChartsData(cb)
+    }
+  },
+  {
+    name: 'ethCall',
+    onEvent: (socket, msg: any, glob, cb): void => {
+      glob.vmR.call(msg, cb)
+    }
+  },
+  {
+    name: 'getKeyValue',
+    onEvent: (socket, msg: any, glob, cb): void => {
+      if (!common.check.isBufferObject(msg, 32)) {
+        cb(common.newError(common.errors.notBuffer), null)
+        return
+      }
+      glob.vmR.getKeyValue(msg, cb)
+    }
+  },
+  {
+    name: 'getCurrentStateRoot',
+    onEvent: (socket, msg: any, glob, cb): void => {
+      if (msg != '')  {
+        cb(common.newError(common.errors.invalidInput), null)
+        return
+      }
+      glob.vmR.getCurrentStateRoot(cb)
+    }
+  },
+  {
+    name: 'getEthToUSD',
+    onEvent: (socket, msg, glob, cb): void => {
+      glob.cacheDB.getString(
+        new Buffer('Iethtousd'),
+        {
+          keyEncoding: 'binary',
+          valueEncoding: 'binary'
+        },
+        (err: Error, result: any) => {
+          if (err == null) {
+            l.debug('EthtoUSD is in cache get')
+            cb(err, result)
+          } else {
+            l.debug('EthtoUSD getting from api')
+            getEthToUSD((err, data) => {
+              l.debug('data', data[0].price_usd)
+              glob.cacheDB.put(
+                new Buffer('Iethtousd'),
+                new Buffer(data[0].price_usd),
+                {
+                  keyEncoding: 'binary',
+                  valueEncoding: 'binary'
+                },
+                (err: Error, result: any) => {
+                  cb(err, result)
+                }
+              )
+              cb(err, result)
+            })
+          }
+        }
+      )
+    }
+  },
+  {
+    name: 'getTransactionPages',
+    onEvent: (socket, msg: any, glob, cb): void => {
+      if (
+        msg.hash &&
+        (!common.check.isBufferObject(msg.hash, 32) || !common.check.isNumber(msg.number))
+      ) {
+        cb(common.newError(common.errors.notBuffer), null)
+        return
+      }
+
+      glob.rdb.getTransactionPages(msg.hash, msg.number, cb)
+    }
+  },
+  {
+    name: 'getAddressTransactionPages',
+    onEvent: (socket, msg: any, glob, cb): void => {
+      if (
+        msg.hash &&
+        (!common.check.isBufferObject(msg.hash, 32) || !common.check.isNumber(msg.number))
+      ) {
+        cb(common.newError(common.errors.notBuffer), null)
+        return
+      }
+
+      if (!common.check.isBufferObject(msg.address, 20)) {
+        cb(common.newError(common.errors.notBuffer), null)
+        return
+      }
+
+      glob.rdb.getAddressTransactionPages(
+        msg.address,
+        msg.hash,
+        msg.number,
+        cb
+      )
+    }
+  }
+]
+
+let onConnection = (
+  socket: SocketIO.Socket,
+  rdb: RethinkDBDataStore,
+  cacheDB: CacheDB,
+  vmR: VmRunner,
+  vmE: any
+) => {
+  events.forEach((event: SocketIOEvent, idx: number) => {
+    socket.on(event.name, (msg: any, cb: Callback) => {
+      event.onEvent(
+        socket,
+        msg,
+        {
+          rdb,
+          vmR,
+          vmE,
+          cacheDB
+        },
+        cb
+      )
     })
   })
 }
 
 async function getEthToUSD(cb: (error: Error, data: any) => void) {
   const res = await fetch('https://api.coinmarketcap.com/v1/ticker/ethereum/')
-  const json = await res.json();
+  const json = await res.json()
   return cb(null, json)
-};
+}
 
 export default onConnection
