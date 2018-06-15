9a994478305d91a6e9a62b9b0bb745aa83ba0a14
Aldo Borrero González <aldo@aldoborrero.com>
2018-06-15 17:44:50 +0200

    Formatted code in LokiDataStore and RedisDataStore

--- src/datastores/providers/LokiDataStore.ts
+++ src/datastores/providers/LokiDataStore.ts
@@ -1,52 +1,65 @@
-import * as loki from 'lokijs'
+import * as Loki from 'lokijs'
 import config from '@/config'
 import { TxModel, BlockModel } from '@/models'
 
-let lokiDB = new loki(config.get('eth_vm_server.data_stores.loki.db_name'), { autosave: true, autosaveInterval: 5000, autoload: true })
-
-let tables = ['transactions', 'blocks']
-
 type CallbackFunction = (data: Array<any>) => void
 
-let setCollections = () => {
-  tables.forEach((item: string, idx: number) => {
-    if (!lokiDB.getCollection(item)) lokiDB.addCollection(item, { unique: ['hash'] }).setTTL(config.get('eth_vm_server.data_stores.loki.ttl.age'), config.get('eth_vm_server.data_stores.loki.ttl.interval'))
+const lokiDB = new Loki(config.get('eth_vm_server.data_stores.loki.db_name'), {
+  autosave: true,
+  autosaveInterval: 5000,
+  autoload: true
+})
+const tables = ['transactions', 'blocks']
+
+const setCollections = () => {
+  tables.forEach((item: string) => {
+    if (!lokiDB.getCollection(item)) {
+      const ttlAge = config.get('eth_vm_server.data_stores.loki.ttl.age')
+      const ttlInterval = config.get('eth_vm_server.data_stores.loki.ttl.interval')
+      lokiDB
+        .addCollection(item, { unique: ['hash'] })
+        .setTTL(ttlAge, ttlInterval)
+    }
   })
 }
 
-let hexify = (obj: any) => {
-  let _obj = Object.assign({}, obj);
+const hexify = (obj: any) => {
+  const o = Object.assign({}, obj)
+  for (var key in o) {
+    if (o.hasOwnProperty(key)) {
+      if (Buffer.isBuffer(o[key])) o[key] = '0x' + o[key].toString('hex')
+    }
+  }
+  return o
+}
+
+const bufferify = (obj: any) => {
+  const _obj = Object.assign({}, obj)
   for (var key in _obj) {
     if (_obj.hasOwnProperty(key)) {
-      if (Buffer.isBuffer(_obj[key])) _obj[key] = '0x' + _obj[key].toString('hex')
+      if (
+        (typeof _obj[key] === 'string' || _obj[key] instanceof String) &&
+        _obj[key].substring(0, 2) == '0x'
+      )
+        _obj[key] = new Buffer(_obj[key].substring(2).toUpperCase(), 'hex')
     }
   }
   return _obj
 }
 
-let bufferify = (obj: any) => {
-  let _obj = Object.assign({}, obj);
-  for (var key in _obj) {
-    if (_obj.hasOwnProperty(key)) {
-      if ((typeof _obj[key] === 'string' || _obj[key] instanceof String) && _obj[key].substring(0, 2) == '0x') _obj[key] = new Buffer(_obj[key].substring(2).toUpperCase(), 'hex')
-    }
-  }
-  return _obj
-}
-
-let processTx = (tx: TxModel) => {
-  let hexed = hexify(tx)
-  let col = lokiDB.getCollection('transactions')
-  var obj = col.by('hash', hexed.hash);
+const processTx = (tx: TxModel) => {
+  const hexed = hexify(tx)
+  const col = lokiDB.getCollection('transactions')
+  const obj = col.by('hash', hexed.hash)
   if (obj) {
-    col.remove(obj);
+    col.remove(obj)
   }
   lokiDB.getCollection('transactions').insert(hexed)
 }
 
 let addTransaction = (tx: TxModel | Array<TxModel>) => {
   if (Array.isArray(tx)) {
-    tx.forEach((tTx) => {
+    tx.forEach(tTx => {
       processTx(tTx)
     })
   } else {
@@ -54,30 +67,40 @@ let addTransaction = (tx: TxModel | Array<TxModel>) => {
   }
 }
 
-let addBlock = (block: BlockModel) => {
-  let hexed = hexify(block)
-  let col = lokiDB.getCollection('blocks')
-  var obj = col.by('hash', hexed.hash);
+const addBlock = (block: BlockModel) => {
+  const hexed = hexify(block)
+  const col = lokiDB.getCollection('blocks')
+  const obj = col.by('hash', hexed.hash)
   if (obj) {
-    col.remove(obj);
+    col.remove(obj)
   }
   lokiDB.getCollection('blocks').insert(hexed)
 }
 
-let getBlocks = (cb: CallbackFunction) => {
-  cb(lokiDB.getCollection('blocks').chain().simplesort('blockNumber').data().map((_block) => {
-    return bufferify(_block)
-  }))
+const getBlocks = (cb: CallbackFunction) => {
+  cb(
+    lokiDB
+      .getCollection('blocks')
+      .chain()
+      .simplesort('blockNumber')
+      .data()
+      .map(_block => {
+        return bufferify(_block)
+      })
+  )
 }
 
 let getTransactions = (cb: CallbackFunction) => {
-  cb(lokiDB.getCollection('transactions').chain().simplesort('blockNumber').data().map((_tx) => {
-    return bufferify(_tx)
-  }))
-}
-
-let thisReturnsANumber = (id: number, name: string): number => {
-  return 0
+  cb(
+    lokiDB
+      .getCollection('transactions')
+      .chain()
+      .simplesort('blockNumber')
+      .data()
+      .map(_tx => {
+        return bufferify(_tx)
+      })
+  )
 }
 
 let initialize = (): void => {
@@ -87,9 +110,9 @@ let initialize = (): void => {
 }
 
 export default {
-  addTransaction,
+  initialize,
   addBlock,
   getBlocks,
-  getTransactions,
-  initialize
+  addTransaction,
+  getTransactions
 }
--- src/datastores/providers/RedisDataStore.ts
+++ src/datastores/providers/RedisDataStore.ts
@@ -2,27 +2,25 @@ import * as Redis from 'ioredis'
 import config from '@/config'
 import { TxModel, BlockModel } from '@/models'
 
+type CallbackFunction = (data: Array<any>) => void
+
 interface ItableCache {
-  transactions: Array<TxModel>;
-  blocks: Array<BlockModel>;
+  transactions: Array<TxModel>
+  blocks: Array<BlockModel>
 }
 
-const resisUrl = config.get('eth_vm_server.data_stores.redis.url')
+const redis = new Redis()
 const socketRows = config.get('eth_vm_server.data_stores.redis.socket_rows')
 
-const redis = new Redis()
-
-let tableCache: ItableCache = {
+const tableCache: ItableCache = {
   transactions: [],
   blocks: []
 }
 
-let tables = {
-  transactions: "transactions",
+const tables = {
+  transactions: 'transactions',
   blocks: 'blocks'
 }
-
-type CallbackFunction = (data: Array<any>) => void
 
 let bufferify = (obj: any): any => {
   for (let key in obj) {
@@ -31,8 +29,10 @@ let bufferify = (obj: any): any => {
         obj[key] = new Buffer(obj[key])
       } else if (Array.isArray(obj[key])) {
         obj[key] = obj[key].map((_item: any) => {
-          if (_item.type && _item.type === 'Buffer') return new Buffer(_item)
-          else return _item
+          if (_item.type && _item.type === 'Buffer') {
+            return new Buffer(_item)
+          }
+          return _item
         })
       }
     }
@@ -41,9 +41,10 @@ let bufferify = (obj: any): any => {
 }
 
 let getArray = (tbName: any, cb: CallbackFunction) => {
-  let tbKey: (keyof ItableCache) = tbName;
-  if (tableCache[tbKey].length) cb(tableCache[tbKey])
-  else {
+  let tbKey: keyof ItableCache = tbName
+  if (tableCache[tbKey].length) {
+    cb(tableCache[tbKey])
+  } else {
     let vals = redis.get(tbName, (err, result) => {
       if (!err && result) {
         let bufferedArr = JSON.parse(result).map((_item: any) => {
@@ -51,33 +52,34 @@ let getArray = (tbName: any, cb: CallbackFunction) => {
         })
         tableCache[tbKey] = bufferedArr
         cb(bufferedArr)
+      } else {
+        cb([])
       }
-      else cb([])
     })
   }
 }
 
 let addTransaction = (tx: TxModel | Array<TxModel>) => {
-  getArray(tables.transactions, (pTxs) => {
+  getArray(tables.transactions, pTxs => {
     if (Array.isArray(tx)) {
-      tx.forEach((tTx) => {
+      tx.forEach(tTx => {
         pTxs.unshift(tTx)
       })
     } else {
       pTxs.unshift(tx)
     }
     if (pTxs.length > socketRows) pTxs = pTxs.slice(0, socketRows)
-    let tbKey: (keyof ItableCache) = "transactions"
+    let tbKey: keyof ItableCache = 'transactions'
     tableCache[tbKey] = pTxs
     redis.set(tables.transactions, JSON.stringify(pTxs))
   })
 }
 
 let addBlock = (block: BlockModel) => {
-  getArray(tables.blocks, (pBlocks) => {
+  getArray(tables.blocks, pBlocks => {
     pBlocks.unshift(block)
     if (pBlocks.length > socketRows) pBlocks = pBlocks.slice(0, socketRows)
-    let tbKey: (keyof ItableCache) = "blocks"
+    let tbKey: keyof ItableCache = 'blocks'
     tableCache[tbKey] = pBlocks
     redis.set(tables.blocks, JSON.stringify(pBlocks))
   })
@@ -89,10 +91,6 @@ let getBlocks = (cb: CallbackFunction) => {
 
 let getTransactions = (cb: CallbackFunction) => {
   getArray(tables.transactions, cb)
-}
-
-let thisReturnsANumber = (id: number, name: string): number => {
-  return 0
 }
 
 let initialize = () => {
