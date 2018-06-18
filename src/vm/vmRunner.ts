1977cc83467afbbdc8f5b1af8865423d98ad1fb0
Aldo Borrero González <aldo@aldoborrero.com>
2018-06-18 23:14:50 +0200

    Refactored VmEngine and VmRunner code

--- src/vm/vmRunner.ts
+++ src/vm/vmRunner.ts
@@ -1,10 +1,11 @@
+import config from '@app/config'
 import { CacheDB } from '@app/datastores'
 import * as VM from '@enkrypt.io/ethereumjs-vm'
 import * as Account from 'ethereumjs-account'
 import * as LRU from 'lru'
 import * as Trie from 'merkle-patricia-tree/secure'
 
-const GAS_LIMIT = '0x4c4b40' // 50000000
+const GAS_LIMIT = config.get('eth.vm.engine.gas_limit')
 
 interface Tx {
   to: string
@@ -16,24 +17,32 @@ const hexToBuffer = (hex: string): Buffer => {
 }
 
 export class VmRunner {
-  public stateTrie: any
-  public codeCache: any
+  private readonly codeCache: any
+  private stateTrie: Trie
 
   constructor(private readonly db: CacheDB) {
     this.codeCache = new LRU(2000)
   }
 
-  public setStateRoot(_hash: Buffer) {
-    const _temp = new Trie(this.db, _hash)
-    this.stateTrie = _temp
+  public setStateRoot(hash: Buffer) {
+    const trie = new Trie(this.db, hash)
+    this.stateTrie = trie
+  }
+
+  public getCurrentStateRoot(): Promise<Buffer> {
+    return new Promise(resolve => {
+      resolve(this.stateTrie.root)
+    })
   }
 
   public call(txs: Tx | Tx[], mCB: (err: Error, result: any) => void) {
-    const _trie = this.stateTrie.copy()
+    const trie = this.stateTrie.copy()
+
     const runCode = (sTree: any, to: Buffer, code: Buffer, gasLimit: string, data: Buffer, _cb: (err: Error, result: any) => void) => {
       const vm = new VM({
         state: sTree
       })
+
       vm.runCode(
         {
           address: to,
@@ -46,16 +55,19 @@ export class VmRunner {
         }
       )
     }
+
     const getResult = (tx: Tx, treeClone: any, cb: (err: Error, result: Buffer) => void) => {
       if (this.codeCache.peek(tx.to)) {
         runCode(treeClone, hexToBuffer(tx.to), this.codeCache.get(tx.to), GAS_LIMIT, hexToBuffer(tx.data), cb)
         return
       }
+
       treeClone.get(hexToBuffer(tx.to), (err: Error, val: Buffer) => {
         if (err) {
           cb(err, null)
           return
         }
+
         const account = new Account(val)
         treeClone.getRaw(account.codeHash, (e: Error, code: Buffer) => {
           if (e) {
@@ -67,11 +79,13 @@ export class VmRunner {
         })
       })
     }
+
     if (Array.isArray(txs)) {
-      const returnArr: Array<{ error: Error; result: any }> = []
+      const returnArr: any[] = []
       let counter = 0
+
       txs.forEach((_tx, _idx) => {
-        getResult(_tx, _trie.copy(), (err: Error, result: any) => {
+        getResult(_tx, trie.copy(), (err: Error, result: any) => {
           counter++
           returnArr[_idx] = { error: err, result }
           if (counter === txs.length) {
@@ -80,33 +94,46 @@ export class VmRunner {
         })
       })
     } else {
-      getResult(txs, _trie, mCB)
+      getResult(txs, trie, mCB)
     }
   }
 
-  public getKeyValue(_key: Buffer, _cb: (err: Error, result: Buffer) => void) {
-    this.db.get(
-      new Buffer(_key),
-      {
-        keyEncoding: 'binary',
-        valueEncoding: 'binary'
-      },
-      _cb
-    )
-  }
+  public getAccount(to: string, cb: (err: Error, result: Buffer) => void) {
+    const trie = this.stateTrie.copy()
+    const buffer = hexToBuffer(to)
 
-  public getCurrentStateRoot(_cb: (err: Error, result: Buffer) => void) {
-    _cb(null, this.stateTrie.root)
-  }
-
-  public getAccount(_to: string, cb: (err: Error, result: Buffer) => void) {
-    const treeClone = this.stateTrie.copy()
-    treeClone.get(hexToBuffer(_to), (err: Error, val: Buffer) => {
+    trie.get(buffer, (err: Error, buffer: Buffer) => {
       if (err) {
         cb(err, null)
         return
       }
-      cb(null, val)
+
+      cb(null, buffer)
+    })
+  }
+
+  private executeOnVm(tree: any, to: Buffer, code: Buffer, gasLimit: string, data: Buffer): Promise<any> {
+    return new Promise((resolve, reject) => {
+      const vm = new VM({
+        state: sTree
+      })
+
+      vm.runCode(
+        {
+          address: to,
+          code,
+          gasLimit,
+          data
+        },
+        (err: Error, result: any) => {
+          if (err) {
+            reject(err)
+            return
+          }
+
+          resolve(result)
+        }
+      )
     })
   }
 }
