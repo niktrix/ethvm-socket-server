2b7587607e800600316628b485e0875b73250271
Aldo Borrero González <aldo@aldoborrero.com>
2018-06-29 17:57:12 +0200

    Added getChartAccountsGrowth, getChartBlockSize and getChartGasLimit events

---
+++ src/server/events/get-chart-block-size.ts
@@ -0,0 +1,16 @@
+import { Callback } from '@app/interfaces'
+import { EthVMServer, SocketEvent } from '@app/server'
+
+// TODO: Validate input with AJV
+// TODO: Create helper function to request time
+const getChartsDataEvent: SocketEvent = {
+  name: 'getChartBlockSize',
+  onEvent: (server: EthVMServer, socket: SocketIO.Socket, msg: any, cb: Callback): void => {
+    server.rdb
+      .getChartAccountsGrowth(new Date(), new Date())
+      .then((result: any): void => cb(null, result))
+      .catch((error: Error): void => cb(error, null))
+  }
+}
+
+export default getChartsDataEvent
--- src/server/events/get-chart-gas-limit.ts
+++ src/server/events/get-chart-gas-limit.ts
@@ -1,11 +1,13 @@
 import { Callback } from '@app/interfaces'
 import { EthVMServer, SocketEvent } from '@app/server'
 
+// TODO: Validate input with AJV
+// TODO: Create helper function to request time
 const getChartsDataEvent: SocketEvent = {
-  name: 'getChartsData',
+  name: 'getChartGasLimit',
   onEvent: (server: EthVMServer, socket: SocketIO.Socket, msg: any, cb: Callback): void => {
     server.rdb
-      .getChartsData()
+      .getChartGasLimit(new Date(), new Date())
       .then((result: any): void => cb(null, result))
       .catch((error: Error): void => cb(error, null))
   }
