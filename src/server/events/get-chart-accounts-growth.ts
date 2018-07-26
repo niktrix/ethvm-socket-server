1745698ff05dc0a9a540e57cb0a9bbeb4823606d
niktrix <nicky.gurbani@gmail.com>
2018-07-26 21:27:05 +0530

    update chart events

--- src/server/events/get-chart-accounts-growth.ts
+++ src/server/events/get-chart-accounts-growth.ts
@@ -1,11 +1,15 @@
+import { DurationValidator } from '@app/helpers'
 import { Callback } from '@app/interfaces'
 import { EthVMServer, SocketEvent } from '@app/server'
 
-// TODO: Validate input with AJV
-// TODO: Create helper function to request time
+// TODO: Create helper function convert selected enum string to time duration
 const getChartsDataEvent: SocketEvent = {
   name: 'getChartAccountsGrowth',
   onEvent: (server: EthVMServer, socket: SocketIO.Socket, msg: any, cb: Callback): void => {
+    const isValid = DurationValidator(msg);
+    if (!isValid) {
+      cb(DurationValidator.errors, null)
+    }
     server.rdb
       .getChartAccountsGrowth(new Date(), new Date())
       .then((result: any): void => cb(null, result))
--- src/server/events/get-chart-tx-fee.ts
+++ src/server/events/get-chart-tx-fee.ts
@@ -1,12 +1,14 @@
-import { logger } from '@app/helpers'
+import { DurationValidator } from '@app/helpers'
 import { Callback } from '@app/interfaces'
 import { EthVMServer, SocketEvent } from '@app/server'
 
-// TODO: Validate input with AJV
-// TODO: Create helper function to request time
-const getChartsAvTxFeeDataEvent: SocketEvent = {
+ const getChartsAvTxFeeDataEvent: SocketEvent = {
   name: 'getChartAvTxFee',
   onEvent: (server: EthVMServer, socket: SocketIO.Socket, msg: any, cb: Callback): void => {
+    const isValid = DurationValidator(msg);
+    if (!isValid) {
+      cb(DurationValidator.errors, null)
+    }
     server.rdb
       .getChartAvTxFee(new Date(), new Date())
       .then((result: any): void => cb(null, result))
