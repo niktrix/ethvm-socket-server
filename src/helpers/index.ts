ce60692092835d543263d5279b246f03ec5151d2
niktrix <nicky.gurbani@gmail.com>
2018-07-26 21:08:34 +0530

    add duration helper

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -2,4 +2,3 @@ export * from '@app/helpers/logger'
 export * from '@app/helpers/eth'
 export * from '@app/helpers/errors'
 export * from '@app/helpers/validate'
-
--- src/helpers/validate.ts
+++ src/helpers/validate.ts
@@ -1,22 +1,37 @@
 import * as Ajv from 'ajv'
 
-const ajv = new Ajv();
+const ajv = new Ajv()
+
 const addressSchema = {
-  "type": "object",
-  "definitions": {},
-  "$schema": "http://json-schema.org/draft-07/schema#",
-  "properties": {
-    "address": {
-      "$id": "/properties/address",
-      "type": "string",
-      "title": "The Address Schema ",
-      "default": "",
-      "examples": [
-        "0x0000000000000"
-      ]
+  type: 'object',
+  definitions: {},
+  $schema: 'http://json-schema.org/draft-07/schema#',
+  properties: {
+    address: {
+      $id: '/properties/address',
+      type: 'string',
+      title: 'The Address Schema ',
+      default: '',
+      examples: ['0x0000000000000']
     }
   }
-};
-const AddressValidator = ajv.compile(addressSchema);
-export {AddressValidator}
+}
 
+const durationSchema = {
+  type: 'object',
+  definitions: {},
+  $schema: 'http://json-schema.org/draft-07/schema#',
+  properties: {
+    duration: {
+      $id: '/properties/duration',
+      type: 'string',
+      enum: ["BEGIN","YEAR","MONTH","DAY"],
+      title: 'The Duration Schema ',
+      default: '',
+    }
+  }
+}
+const AddressValidator = ajv.compile(addressSchema)
+const DurationValidator = ajv.compile(durationSchema)
+
+export { AddressValidator, DurationValidator}
