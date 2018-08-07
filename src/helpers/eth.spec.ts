4d76f12dd63115b2da057a78b5fba6a6ac082e86
Aldo Borrero González <aldo@aldoborrero.com>
2018-08-07 20:34:06 +0200

    Reworked eth and added proper tests to isBuffer method

--- src/helpers/eth.spec.ts
+++ src/helpers/eth.spec.ts
@@ -1,9 +1,64 @@
 import { expect } from 'chai'
+import { isBuffer } from './eth'
 
 describe('helpers.eth', () => {
-  describe('start() method', () => {
-    it('should return true', () => {
-      expect(true).to.be.true
+  describe('isBufferObject()', () => {
+    it('should validate correct buffer objects', () => {
+      const inputs = [
+        {
+          buffer: Buffer.alloc(0),
+          length: 0
+        },
+        {
+          buffer: new Buffer(''),
+          length: 0
+        },
+        {
+          buffer: Buffer.from('b903239f8543d04b5dc1ba6579132b143087c68db1b2168786408fcbce568238', 'hex'),
+          length: 32
+        },
+        {
+          buffer: Buffer.from('b60e8dd61c5d32be8058bb8eb970870f07233155', 'hex'),
+          length: 20
+        }
+      ]
+      inputs.forEach(input => {
+        const isValid = isBuffer(input.buffer, input.length)
+        expect(isValid).to.be.true
+      })
+    })
+
+    it('should not validate incorrect buffer objects', () => {
+      const inputs = [
+        {
+          buffer: Buffer.alloc(0),
+          length: 1
+        },
+        {
+          buffer: new Buffer('cheese'),
+          length: 0
+        },
+        {
+          buffer: Buffer.from('b903239f8543d04b5dc1ba6579132b143087c68db1b2168786408fcbce', 'hex'),
+          length: 32
+        },
+        {
+          buffer: Buffer.from('b60e8dd61c5d32be8058bb8eb970870f075', 'hex'),
+          length: 20
+        },
+        {
+          buffer: 'another thing',
+          length: 20
+        },
+        {
+          buffer: Buffer.alloc(0),
+          length: -12
+        }
+      ]
+      inputs.forEach(input => {
+        const isValid = isBuffer(input.buffer, input.length)
+        expect(isValid).to.be.false
+      })
     })
   })
 })
--- src/helpers/eth.ts
+++ src/helpers/eth.ts
@@ -1,30 +1,23 @@
-import _ from 'lodash'
+export const isValidHash = (hash: string): boolean => /^(0x)?([A-Fa-f0-9]{64})$/.test(hash)
 
-const validateByteArray = (arr: number[]): boolean => arr.some((item: number) => !_.isNumber(item) || item < 0 || item > 255)
+export const isBuffer = (item: any, length: number = 0): boolean => Buffer.isBuffer(item) && (item as Buffer).length === length
 
-export const eth = {
-  isValidHash: (hash: string): boolean => /^(0x)?([A-Fa-f0-9]{64})$/.test(hash),
+export const hexToBuffer = (hex: string): Buffer => Buffer.from(hex.toLowerCase().replace('0x', ''), 'hex')
 
-  hexToBuffer: (hex: string): Buffer => Buffer.from(hex.toLowerCase().replace('0x', ''), 'hex'),
-
-  isBufferObject: (item: any, length: number): boolean =>
-    item.type && item.type === 'Buffer' && item.data && _.isArray(item.data) && validateByteArray(item.data) && item.data.length === length,
-
-  bufferify: (obj: any): any => {
-    for (const key in obj) {
-      if (obj.hasOwnProperty(key) && obj[key]) {
-        if (obj[key].type && obj[key].type === 'Buffer') {
-          obj[key] = new Buffer(obj[key])
-        } else if (Array.isArray(obj[key])) {
-          obj[key] = obj[key].map((_item: any) => {
-            if (_item.type && _item.type === 'Buffer') {
-              return new Buffer(_item)
-            }
-            return _item
-          })
-        }
+export const bufferify = (obj: any): any => {
+  for (const key in obj) {
+    if (obj.hasOwnProperty(key) && obj[key]) {
+      if (obj[key].type && obj[key].type === 'Buffer') {
+        obj[key] = new Buffer(obj[key])
+      } else if (Array.isArray(obj[key])) {
+        obj[key] = obj[key].map((_item: any) => {
+          if (_item.type && _item.type === 'Buffer') {
+            return new Buffer(_item)
+          }
+          return _item
+        })
       }
     }
-    return obj
   }
+  return obj
 }
