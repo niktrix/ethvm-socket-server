d31c916d8e11d9bd09796acec1b64ec006c1d503
Aldo Borrero González <aldo@aldoborrero.com>
2018-08-08 13:15:45 +0200

    Improved toDatePeriod fn and added tests

--- src/models/helpers/durations.ts
+++ src/models/helpers/durations.ts
@@ -1,10 +1,12 @@
-export const toDate = (duration: string) => {
-  let from
+export const toDatePeriods = (duration: string) => {
   const to = new Date()
+  // Always at the end of the day
+  to.setHours(23,59,59,999)
 
+  let from
   switch (duration) {
     case 'ALL':
-      from = new Date(1900) // Far away date
+      from = new Date(1970, 0, 1) // Far away date, we support different networks
       break
     case 'YEAR':
       from = new Date(to.getFullYear(), 0, 1)
@@ -15,7 +17,12 @@ export const toDate = (duration: string) => {
     case 'DAY':
       from = new Date()
       break
+    default:
+      throw new Error('Invalid range specified')
   }
+
+  // Always at the start of the day
+  from.setHours(0,0,0,0)
 
   return {
     from,
